bfdb65d7a227f4a28c970af5b98aee139c4fec22
pedro <pedro.phdois@gmail.com>
2020-01-30 10:20:29 -0300

    me.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import me from "./me";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: { me },
+  getters: {
+    getMe: state => state.me
+  },
   mutations: {},
   actions: {},
   modules: {}
---
+++ src/store/me.ts
@@ -0,0 +1,43 @@
+const me = {
+  personalInformation: {
+    name: "Pedro Henrique Silva Rosa",
+    birthData: "1996-04-15",
+    address:
+      "2897 Av Brigadeiro Luis Antonio 2897, Apt 52, São Paulo, SP, 01401000, Brazil",
+    phone: "(12) 988274202",
+    email: "pedro.phdois@gmail.com",
+    website: "https://pedroatem.github.io/me",
+    github: "https://github.com/pedroAtem/"
+  },
+  objective: "",
+  academicEducation: [
+    {
+      type: "College",
+      name: "FATEC SJC"
+    }
+  ],
+  professionalExperience: [
+    /**
+     * internship or work
+     */
+    {
+      type: "internship",
+      company: "Suporte Computadores",
+      fromDate: "",
+      toDate: ""
+    }
+  ],
+  languages: [
+    {
+      name: "Portuguese",
+      level: "Native Speaker"
+    },
+    {
+      name: "English",
+      level: "Intermediate"
+    }
+  ],
+  skills: ["Javascript", "Vue", "Angular", "React", "Html", "Css"]
+};
+
+export default me;
