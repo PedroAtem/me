058fdfce261573c82335567a8fb3be7490408b10
pedro <pedro.phdois@gmail.com>
2020-01-30 09:50:21 -0300

    2e982b1

---
+++ precache-manifest.3230d09d3681eff9c633671f0faab454.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0debde9f63326eaae85",
+    "url": "/me/css/app.8c0a3192.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/me/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d8c621afebcb96454361c21af2a4e277",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "587ce9a4cf53d23942ec",
+    "url": "/me/js/about.a2476f47.js"
+  },
+  {
+    "revision": "d0debde9f63326eaae85",
+    "url": "/me/js/app.5fd0ce2b.js"
+  },
+  {
+    "revision": "961d611e0a39ca7b033f",
+    "url": "/me/js/chunk-vendors.9f1ed624.js"
+  },
+  {
+    "revision": "0ecb27366919d24a66939294fe10dcdf",
+    "url": "/me/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/me/robots.txt"
+  }
+]);
